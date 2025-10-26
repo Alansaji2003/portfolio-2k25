1dad4066f2268c9f46082aace3c9dbc87cf72452
Alan Saji <rockstaralansaji@gmail.com>
2025-10-26 11:24:20 +0530

    spelling and routing fix

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -53,7 +53,7 @@ const About = () => {
             <p className="headtext">Tech Stack</p>
             <p className="subtext">
               I specialize in a variety of languages, frameworks, and tools that
-              allow me to build robust and scalable applications.
+              help me to build robust and scalable applications.
             </p>
           </div>
           <div className="absolute md:top-40 top-45 bottom-0 md:inset-y-9 w-full h-full lg:start-[50%] md:scale-125 z-0">
